dece2de79eb16c41dea5363b5ca300942cd0f77a
Brian Kalbermatter <tejant6@hotmail.com>
2025-05-06 17:57:27 -0300

    Agregando y aprendiendo C en linux

---
+++ C_Unix/c.c
@@ -0,0 +1,43 @@
+//Se llama a esta biblioteca para poder usar printf
+#include <stdio.h>
+//Funcion principal
+int main(){
+    /*Esto es un comentario de varias lineas*/
+    printf("Hola mundo\n");
+    printf("Hola mundo");
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
