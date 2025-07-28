f1823d7e00137891c5a4eb782f3eb6d44e8251bd
Brian Kalbermatter <tejant6@hotmail.com>
2025-07-28 06:30:40 -0300

    Lunes 28, examen de LED, Temas estudiado, conjuntos, graficas, resta por complemento

---
+++ MyFirst_OS/BasesParaPascal/Pascal/PascalApp/descubriendo.pas
@@ -0,0 +1,42 @@
+//Nombre del programa
+program descubriendo;
+
+//Uso de las librerias
+uses crt
+
+//Constantes Globales
+const 
+    PI = 3.14159265358979323846;
+//Variables Globales
+var
+    nombre: string;
+    edad: integer;
+
+//Funciones Declaraciones, si hay algunos
+//function Persona(Humano: boolean): string;
+//begin
+//    if Humano = True then
+//        Persona := 'Soy un Humano';
+//    else
+//        Persona := 'No soy Humano';
+//end;
+
+//Procedimientos
+procedure Saludar(nombre: string);
+begin
+    WriteLn(nombre);
+end;
+
+procedure Edad(edad: integer);
+begin
+    WriteLn(edad);
+end;
+
+
+
+begin//Inicio del programa principal
+    WriteLn(Persona(Humano));
+    Saludar('Juan');
+    Edad(20);
+    WriteLn(PI);
+end.//Fin del programa principal
